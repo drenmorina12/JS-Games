d31d9bc7ebbb58703f286a7d8228052a7e15bfe7
drenmorina12 <drenmorina321@gmail.com>
2023-09-27 15:25:12 +0200

    Add existing Memory Game and Rock-Paper-Scissors game files to GitHub.

---
+++ MEMORY-GAME/app.js
@@ -0,0 +1,128 @@
+const cardArray = [
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    }
+]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+const gridDisplay = document.querySelector('#grid')
+const resultDisplay = document.querySelector('#result')
+
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
+let isChecking = false;
+
+function createBoard(){
+    for (let i = 0; i < cardArray.length; i++){
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank-1.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        gridDisplay.append(card)
+    }
+}
+
+createBoard()
+
+function checkMatch() {
+    const cards = document.querySelectorAll('#grid img')
+    const optionOneId = cardsChosenIds[0]
+    const optionTwoId = cardsChosenIds[1]
+
+    console.log(optionOneId)
+    console.log(optionTwoId)
+    console.log(cardsChosen[0])
+    console.log(cardsChosen[1])    
+    
+    if ((cardsChosen[0] === cardsChosen[1]) && (optionOneId != optionTwoId)){
+        alert('You found a match')
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen[0])
+    }
+    else {
+        cards[optionOneId].setAttribute('src', 'images/blank-1.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank-1.png')
+    }
+    resultDisplay.innerHTML = cardsWon.length
+
+    cardsChosen = []
+    cardsChosenIds = []
+
+    if (cardsWon.length ===cardArray.length/2){
+        resultDisplay.innerHTML = "You won !!!"
+    }
+
+    isChecking = false
+}
+
+
+function flipCard() {
+    if (isChecking){
+        return
+    }
+
+    const cardId = this.getAttribute('data-id')
+    
+    if (cardsChosenIds[0] !== cardId){
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenIds.push(cardId)
+    }
+
+    console.log('Clicked', cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2){
+        isChecking = true
+        setTimeout(checkMatch, 500)
+    }
+
+}
---
+++ ROCK-PAPER-SCISSORS/app.js
@@ -0,0 +1,50 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const playerChoiceDisplay = document.getElementById('player-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+
+let playerChoice
+let computerChoice
+let result
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    playerChoice = e.target.id
+    playerChoiceDisplay.innerHTML = playerChoice
+    generateComputerChoice()
+    getResult()
+}))
+
+function generateComputerChoice() {
+
+    const randomChoice = possibleChoices[Math.floor(Math.random() * possibleChoices.length)].id
+    computerChoice = randomChoice
+
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+    if (computerChoice === playerChoice){
+        result = "It's a Draw"
+    }
+
+    else if (computerChoice === 'rock' && playerChoice === 'paper'){
+        result = "You won"
+    }
+
+
+
+    else if (computerChoice === 'paper' && playerChoice === 'scissors'){
+        result = "You won"
+    }
+
+
+
+    else if (computerChoice === 'scissors' && playerChoice === 'rock'){
+        result = "You won"
+    }
+
+    else
+        result = "You lose"
+ 
+    resultDisplay.innerHTML = result
+}
