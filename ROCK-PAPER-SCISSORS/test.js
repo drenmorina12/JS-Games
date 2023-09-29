10cd05f0ff7f3959fb72a0162816098c79a0e24f
drenmorina12 <drenmorina321@gmail.com>
2023-09-29 03:25:34 +0200

    Display player and computer move on clcik. Finish generataComputerChoice function. Add getResult function.

--- ROCK-PAPER-SCISSORS/test.js
+++ ROCK-PAPER-SCISSORS/test.js
@@ -1,24 +1,61 @@
-const playerChoiceDisplay = document.getElementsByClassName('.player-choice')
-const computerChoiceDisplay = document.getElementsByClassName('.computer-choice')
+const playerChoiceDisplay = document.querySelector('.player-choice')
+const computerChoiceDisplay = document.querySelector('.computer-choice')
 const possibleChoices = document.querySelectorAll('.option')
 
 let playerChoice
 let computerChoice
 let result
+let playerMove
+let computerMove
+
+console.log(possibleChoices)
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     playerChoice = e.target.id
-    //display computer choice
-    // call functions neccesary when event is clicked
+    playerMove = document.createElement('img')
+    playerMove.setAttribute('id', playerChoice)
+    playerMove.setAttribute('src', 'images/' + playerChoice + '.png')
+    playerChoiceDisplay.append(playerMove)
+    generateComputerChoice()
+
 }))
+
 
 function generateComputerChoice() {
     const randomChoice = possibleChoices[Math.floor(Math.random() * possibleChoices.length)].id
     computerChoice = randomChoice
-
+    computerMove = document.createElement('img')
+    computerMove.setAttribute('id', computerChoice)
+    computerMove.setAttribute('src', ('images/' + computerChoice + '.png'))
+    // computerMove.setAttribute('src', 'images/paper.png')
+    computerChoiceDisplay.append(computerMove)
     // display computer choice
+    console.log('suiii')
 }
 
 function getResult() {
+    if (computerChoice === playerChoice){
+        result = "It's a Draw"
+    }
 
+    else if (computerChoice === 'rock' && playerChoice === 'paper'){
+        result = "You won"
+    }
+
+
+
+    else if (computerChoice === 'paper' && playerChoice === 'scissors'){
+        result = "You won"
+    }
+
+
+
+    else if (computerChoice === 'scissors' && playerChoice === 'rock'){
+        result = "You won"
+    }
+
+    else
+        result = "You lose"
+ 
+    resultDisplay.innerHTML = result
 }
